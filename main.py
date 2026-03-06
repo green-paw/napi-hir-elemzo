04d9d810b23e5e58d200262dc1eddafaff066d84
green-paw <tothlevente80@gmail.com>
2026-03-06 15:42:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 # Gemini konfigurálása
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
+model = genai.GenerativeModel('models/gemini-2.0-flash')
 
 def send_telegram(text):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"

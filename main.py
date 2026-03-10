fd8ee86358a10f0592af3788896d199afa291b17
green-paw <tothlevente80@gmail.com>
2026-03-10 09:18:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import config
 import feedparser
 import re
 import telebot # Feltételezve, hogy a pyTelegramBotAPI-t használod
-from google import generativeai as genai
+from google import genai
 
 # --- Konfiguráció inicializálása ---
 genai.configure(api_key=config.GOOGLE_API_KEY)

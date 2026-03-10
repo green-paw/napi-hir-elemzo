dad696ab79db974d48223b8dddb538b789e1788c
green-paw <tothlevente80@gmail.com>
2026-03-10 08:22:41 +0100

    Create rss_sources.py

---
+++ rss_sources.py
@@ -0,0 +1,13 @@
+RSS_SOURCES = {
+    # Nemzetközi források (Angol)
+    "reuters": "https://www.reutersagency.com/feed/?best-topics=political-general&post_type=best",
+    "bloomberg": "https://www.bloomberg.com/feeds/bview/rss",
+    
+    # Hazai források (Magyar)
+    "telex": "https://telex.hu/rss",
+    "portfolio": "https://www.portfolio.hu/rss/gazdasag.xml",
+    "24hu": "https://24.hu/feed/",
+    
+    # A jól bevált Google News RSS (Kulcsszavas/Helyi)
+    "google_news": "https://news.google.com/rss/search?q=hungary+politics+OR+economy&hl=hu&gl=HU&ceid=HU:hu"
+}

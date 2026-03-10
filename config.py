aa583a45fd4d015c57a7a24952c2ba7f5a41976f
green-paw <tothlevente80@gmail.com>
2026-03-10 15:10:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,31 @@ RSS_SOURCES = {
     # Nemzetközi források (Angol)
     "reuters": "https://news.google.com/rss/search?q=site:reuters.com+when:24h&hl=en-US&gl=US&ceid=US:en",
     "bloomberg": "https://feeds.bloomberg.com/markets/news.rss",
+
+    # Globális - Konzervatív / Jobboldali (USA/UK)
+    "fox_news": "https://news.google.com/rss/search?q=site:foxnews.com+when:24h&hl=en-US&gl=US&ceid=US:en",
+    "telegraph": "https://news.google.com/rss/search?q=site:telegraph.co.uk+when:24h&hl=en-GB&gl=GB&ceid=GB:en",
+
+    # Globális - Liberális / Baloldali (USA/UK)
+    "guardian": "https://www.theguardian.com/world/rss",
+    "cnn": "https://news.google.com/rss/search?q=site:cnn.com+when:24h&hl=en-US&gl=US&ceid=US:en",
+
+    # Globális - Piac és Geopolitika (Kiemelten stabil források)
+    "financial_times": "https://news.google.com/rss/search?q=site:ft.com+when:24h&hl=en-US&gl=US&ceid=US:en",
+    "al_jazeera": "https://www.aljazeera.com/xml/rss/all.xml"
     
-    # Hazai források (Magyar)
+    # Hazai - Jobboldali / Kormánypárti perspektíva
+    "magyar_nemzet": "https://magyarnemzet.hu/feed",
+    "mandiner": "https://mandiner.hu/rss",
+
+    # Hazai - Baloldali / Liberális / Kritikai perspektíva
+    "hvg": "https://hvg.hu/rss",
+    "444": "https://444.hu/feed",
+
+    # Hazai - Gazdasági (Alternatív a Portfolio mellé)
+    "vg": "https://www.vg.hu/feed"
+
+    # hazai egyéb
     "telex": "https://telex.hu/rss",
     "portfolio": "https://www.portfolio.hu/rss/gazdasag.xml",
     "24hu": "https://24.hu/feed/",

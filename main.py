59449ba96918f4952cdeed38e3b3b0bbf0447cd3
green-paw <tothlevente80@gmail.com>
2026-03-17 10:33:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import output_handler
 from gemini_handler import (
     get_strategic_topics, validate_news_clusters, 
     generate_event_summary, get_gemini_embeddings, 
-    translate_if_needed, ClusterResult
+    translate_if_needed, MultiClusterResponse
 )
 
 from rss_handler import fetch_news

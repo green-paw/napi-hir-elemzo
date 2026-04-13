dd064c940906b64eb7a3e0514aff463967ec3ea9
green-paw <tothlevente80@gmail.com>
2026-04-13 16:21:15 +0200

    Update source.py

--- source.py
+++ source.py
@@ -430,7 +430,7 @@ def iterative_clustering(news_items: List[NewsItem], start_th: float = 0.05, end
         clusters = create_clusters_by_embedding(remaining_items, threshold=current_th)
         still_remaining = []
         for cluster in clusters:
-            if len(cluster) >= critical_mass:
+            if len(cluster.items) >= critical_mass:
                 final_clusters.append(cluster)
             else:
                 still_remaining.extend(cluster.items)
